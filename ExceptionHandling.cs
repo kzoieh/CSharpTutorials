cb0af7298722acbb726d79563f5cb8acfd0a804d
Kasturi <kasturi.4220@gmail.com>
2025-12-09 16:41:14 +0530

    Create ExceptionHandling.cs

---
+++ ExceptionHandling.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ExceptionHandling
+{
+    class ExpectionHandling
+    {
+        static void Main(string[] args)
+        {
+            //EXCEPTION HANDLING 
+        }
+    }
+}
